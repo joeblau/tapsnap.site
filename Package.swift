466b79f48a0d3bab420f35136be14f2e4eb84007
Joe Blau <im@joeblau.com>
2020-06-15 20:41:31 -0700

    Upgrade to swift 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
@@ -8,7 +8,7 @@ let package = Package(
         .executable(name: "Tapsnap", targets: ["Tapsnap"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.1.0"),
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.1.0")
     ],
     targets: [
         .target(
